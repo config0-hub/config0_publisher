a1b2ba9fceac811b35db8e27e5f33dd4ca9c94b8
gary <gary>
2024-01-30 17:08:29 +0800

    rendered and updated 01-30-2024-17-08

--- config0_publisher/resource/aws.py
+++ config0_publisher/resource/aws.py
@@ -36,8 +36,7 @@ class TFCmdOnAWS(object):
                 'export DNS=True'
                 f'if [ ! -z "$DNE" ]; then curl -L -s https://releases.hashicorp.com/terraform/{tf_version}/terraform_{tf_version}_linux_amd64.zip -o terraform.zip; fi',
                 'unzip terraform.zip',
-                'chmod 777 terraform',
-                'mv terraform $TF_PATH',
+                'chmod 777 $TF_PATH',
                 'ls -al $TF_PATH'
             ])
 
@@ -50,6 +49,8 @@ class TFCmdOnAWS(object):
             'ls -al /tmp',
             'ls -al /tmp/build',
             'ls -al /tmp/build/var/tmp/terraform',
+            'chmod 777 $TF_PATH',
+            'ls -al $TF_PATH',
             'echo "#######################################" && cat $TMPDIR/build_env_vars.env && echo "#######################################"'  # testtest456
         ]
 
