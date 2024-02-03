3bce026f4f90512f846a49a803e9dac6b1077847
gary <gary>
2024-02-03 16:12:34 +0800

    rendered and updated 02-03-2024-16-12

--- config0_publisher/resource/aws.py
+++ config0_publisher/resource/aws.py
@@ -14,7 +14,7 @@ class TFCmdOnAWS(object):
     def get_clean_dirs(self):
 
         cmds = [
-            f'rm -rf $TMPDIR/$STATEFUL_ID || echo env file already removed',
+            f'rm -rf $TMPDIR/config0 || echo env file already removed',
         ]
 
     def get_tf_install(self,tf_bucket_path,tf_version="1.3.7"):
@@ -29,18 +29,18 @@ class TFCmdOnAWS(object):
 
         if tf_bucket_path:
             cmds.extend([
-                f'(cd $TMPDIR && aws s3 cp {tf_bucket_path} terraform.zip --quiet) || \
-                   cd $TMPDIR && curl -L -s https://releases.hashicorp.com/terraform/{tf_version}/terraform_{tf_version}_linux_amd64.zip -o terraform.zip && \
-                   cd $TMPDIR && aws s3 cp terraform.zip {tf_bucket_path} --quiet'
+                f'(cd $TMPDIR/download && aws s3 cp {tf_bucket_path} terraform.zip --quiet) || \
+                   cd $TMPDIR/download && curl -L -s https://releases.hashicorp.com/terraform/{tf_version}/terraform_{tf_version}_linux_amd64.zip -o terraform.zip && \
+                   cd $TMPDIR/download && aws s3 cp terraform.zip {tf_bucket_path} --quiet'
             ])
         else:
             cmds.extend([
-                f'cd $TMPDIR && curl -L -s https://releases.hashicorp.com/terraform/{tf_version}/terraform_{tf_version}_linux_amd64.zip -o terraform.zip'
+                f'cd $TMPDIR/download && curl -L -s https://releases.hashicorp.com/terraform/{tf_version}/terraform_{tf_version}_linux_amd64.zip -o terraform.zip'
             ])
 
         cmds.extend([
-            'cd $TMPDIR && unzip terraform.zip',
-            'cd $TMPDIR && mv terraform $TF_PATH > /dev/null || exit 0',
+            'cd $TMPDIR/download && unzip terraform.zip',
+            'cd $TMPDIR/download && mv terraform $TF_PATH > /dev/null || exit 0',
             'chmod 777 $TF_PATH'
             ]
         )
@@ -54,24 +54,24 @@ class TFCmdOnAWS(object):
                                        self.envfile)
             cmds = [
                 f'rm -rf {envfile_env} || echo env file already removed',
-                f'if [ -f {envfile_env}.enc ]; then cat {envfile_env}.enc | openssl enc -d -aes-256-cbc -pbkdf2 -iter 100000 -pass pass:$STATEFUL_ID -base64 | base64 -d > $TMPDIR/$STATEFUL_ID/{self.envfile}; fi'
+                f'if [ -f {envfile_env}.enc ]; then cat {envfile_env}.enc | openssl enc -d -aes-256-cbc -pbkdf2 -iter 100000 -pass pass:$STATEFUL_ID -base64 | base64 -d > $TMPDIR/config0/$STATEFUL_ID/{self.envfile}; fi'
              ]
         else:
             envfile_env = os.path.join(self.app_dir,
                                        self.envfile)
             cmds = [
-                f'rm -rf $TMPDIR/$STATEFUL_ID/{envfile_env} || echo env file already removed',
-                f'/tmp/decrypt -s $STATEFUL_ID -d $TMPDIR/$STATEFUL_ID/{self.envfile} -e $TMPDIR/$STATEFUL_ID/build/{envfile_env}.enc'
+                f'rm -rf $TMPDIR/config0/$STATEFUL_ID/{envfile_env} || echo env file already removed',
+                f'/tmp/decrypt -s $STATEFUL_ID -d $TMPDIR/config0/$STATEFUL_ID/{self.envfile} -e $TMPDIR/config0/$STATEFUL_ID/build/{envfile_env}.enc'
             ]
 
         return cmds
 
     def get_src_buildenv_vars(self):
 
-        #cmd = f'cd /$TMPDIR/$STATEFUL_ID/; . ./{self.envfile}'
+        #cmd = f'cd /$TMPDIR/config0/$STATEFUL_ID/; . ./{self.envfile}'
 
         cmds = [
-            f'if [ -f /$TMPDIR/$STATEFUL_ID/{self.envfile} ]; then cd /$TMPDIR/$STATEFUL_ID/; . ./{self.envfile} ; fi'
+            f'if [ -f /$TMPDIR/config0/$STATEFUL_ID/{self.envfile} ]; then cd /$TMPDIR/config0/$STATEFUL_ID/; . ./{self.envfile} ; fi'
         ]
 
         return cmds
@@ -81,10 +81,10 @@ class TFCmdOnAWS(object):
         # this does not work in lambda
         # so we won't use it for now
         cmds = [
-          '(cd $TMPDIR/$STATEFUL_ID/build/$APP_DIR && aws s3 cp s3://$REMOTE_STATEFUL_BUCKET/$STATEFUL_ID.tfstate terraform-tfstate) || echo "s3://$REMOTE_STATEFUL_BUCKET/$STATEFUL_ID.tfstate does not exists"',
-          'cd $TMPDIR/$STATEFUL_ID/build && zip -r $TMPDIR/$STATEFUL_ID.zip . ',
-          'cd $TMPDIR/$STATEFUL_ID/build && aws s3 cp $TMPDIR/$STATEFUL_ID.zip s3://$REMOTE_STATEFUL_BUCKET/$STATEFUL_ID',
-          'cd $TMPDIR/$STATEFUL_ID/build && rm -rf $TMPDIR/$STATEFUL_ID.zip ',
+          '(cd $TMPDIR/config0/$STATEFUL_ID/build/$APP_DIR && aws s3 cp s3://$REMOTE_STATEFUL_BUCKET/$STATEFUL_ID.tfstate terraform-tfstate) || echo "s3://$REMOTE_STATEFUL_BUCKET/$STATEFUL_ID.tfstate does not exists"',
+          'cd $TMPDIR/config0/$STATEFUL_ID/build && zip -r $TMPDIR/config0/$STATEFUL_ID.zip . ',
+          'cd $TMPDIR/config0/$STATEFUL_ID/build && aws s3 cp $TMPDIR/config0/$STATEFUL_ID.zip s3://$REMOTE_STATEFUL_BUCKET/$STATEFUL_ID',
+          'cd $TMPDIR/config0/$STATEFUL_ID/build && rm -rf $TMPDIR/config0/$STATEFUL_ID.zip ',
           'echo "# terraform files uploaded s3://$REMOTE_STATEFUL_BUCKET/$STATEFUL_ID" '
         ]
 
@@ -92,45 +92,45 @@ class TFCmdOnAWS(object):
 
     def s3_to_local(self):
 
-        cmds = [ 'aws s3 cp s3://$REMOTE_STATEFUL_BUCKET/$STATEFUL_ID $TMPDIR/$STATEFUL_ID/$STATEFUL_ID.zip --quiet',
-                 'rm -rf $TMPDIR/$STATEFUL_ID/build || echo "stateful already removed"',
-                 'mkdir -p $TMPDIR/$STATEFUL_ID/build',
-                 'unzip -o $TMPDIR/$STATEFUL_ID/$STATEFUL_ID.zip -d $TMPDIR/$STATEFUL_ID/build',
-                 'rm -rf $TMPDIR/$STATEFUL_ID/$STATEFUL_ID.zip'
+        cmds = [ 'aws s3 cp s3://$REMOTE_STATEFUL_BUCKET/$STATEFUL_ID $TMPDIR/config0/$STATEFUL_ID/$STATEFUL_ID.zip --quiet',
+                 'rm -rf $TMPDIR/config0/$STATEFUL_ID/build || echo "stateful already removed"',
+                 'mkdir -p $TMPDIR/config0/$STATEFUL_ID/build',
+                 'unzip -o $TMPDIR/config0/$STATEFUL_ID/$STATEFUL_ID.zip -d $TMPDIR/config0/$STATEFUL_ID/build',
+                 'rm -rf $TMPDIR/config0/$STATEFUL_ID/$STATEFUL_ID.zip'
         ]
 
         return cmds
 
     def get_tf_apply(self):
 
-        #'(cd $TMPDIR/build/$APP_DIR && $TF_PATH init) || (cd $TMPDIR/build/$APP_DIR && $TF_PATH init --migrate-state  -force-copy)',
+        #'(cd $TMPDIR/config0/build/$APP_DIR && $TF_PATH init) || (cd $TMPDIR/config0/build/$APP_DIR && $TF_PATH init --migrate-state  -force-copy)',
         cmds = [
-            'cd $TMPDIR/$STATEFUL_ID/build/$APP_DIR && $TF_PATH init',
-            'cd $TMPDIR/$STATEFUL_ID/build/$APP_DIR && $TF_PATH plan -out=tfplan',
-            'cd $TMPDIR/$STATEFUL_ID/build/$APP_DIR && $TF_PATH apply tfplan || export FAILED=true',
-            'cd $TMPDIR/$STATEFUL_ID/build/$APP_DIR && if [ ! -z "$FAILED" ]; then cd $TMPDIR/$STATEFUL_ID/build/$APP_DIR && $TF_PATH destroy -auto-approve; fi',
-            'cd $TMPDIR/$STATEFUL_ID/build/$APP_DIR && if [ ! -z "$FAILED" ]; then echo "terraform apply failed - destroying and exiting with failed" && exit 9; fi'
+            'cd $TMPDIR/config0/$STATEFUL_ID/build/$APP_DIR && $TF_PATH init',
+            'cd $TMPDIR/config0/$STATEFUL_ID/build/$APP_DIR && $TF_PATH plan -out=tfplan',
+            'cd $TMPDIR/config0/$STATEFUL_ID/build/$APP_DIR && $TF_PATH apply tfplan || export FAILED=true',
+            'cd $TMPDIR/config0/$STATEFUL_ID/build/$APP_DIR && if [ ! -z "$FAILED" ]; then cd $TMPDIR/config0/$STATEFUL_ID/build/$APP_DIR && $TF_PATH destroy -auto-approve; fi',
+            'cd $TMPDIR/config0/$STATEFUL_ID/build/$APP_DIR && if [ ! -z "$FAILED" ]; then echo "terraform apply failed - destroying and exiting with failed" && exit 9; fi'
         ]
 
         return cmds
 
     def get_tf_destroy(self):
 
-        #'(cd $TMPDIR/$STATEFUL_ID/build/$APP_DIR && $TF_PATH init) || (cd $TMPDIR/$STATEFUL_ID/build/$APP_DIR && $TF_PATH init --migrate-state  -force-copy)',
+        #'(cd $TMPDIR/config0/$STATEFUL_ID/build/$APP_DIR && $TF_PATH init) || (cd $TMPDIR/config0/$STATEFUL_ID/build/$APP_DIR && $TF_PATH init --migrate-state  -force-copy)',
         cmds = [
-          'cd $TMPDIR/$STATEFUL_ID/build/$APP_DIR && $TF_PATH init',
-          'cd $TMPDIR/$STATEFUL_ID/build/$APP_DIR && $TF_PATH destroy -auto-approve'
+          'cd $TMPDIR/config0/$STATEFUL_ID/build/$APP_DIR && $TF_PATH init',
+          'cd $TMPDIR/config0/$STATEFUL_ID/build/$APP_DIR && $TF_PATH destroy -auto-approve'
         ]
 
         return cmds
 
     def get_tf_validate(self):
 
-        #'(cd $TMPDIR/$STATEFUL_ID/build/$APP_DIR && $TF_PATH init) || (cd $TMPDIR/$STATEFUL_ID/build/$APP_DIR && $TF_PATH init --migrate-state  -force-copy)',
+        #'(cd $TMPDIR/config0/$STATEFUL_ID/build/$APP_DIR && $TF_PATH init) || (cd $TMPDIR/config0/$STATEFUL_ID/build/$APP_DIR && $TF_PATH init --migrate-state  -force-copy)',
         cmds = [
-            'cd $TMPDIR/$STATEFUL_ID/build/$APP_DIR && $TF_PATH init',
-            'cd $TMPDIR/$STATEFUL_ID/build/$APP_DIR && $TF_PATH refresh',
-            'cd $TMPDIR/$STATEFUL_ID/build/$APP_DIR && $TF_PATH plan -detailed-exitcode'
+            'cd $TMPDIR/config0/$STATEFUL_ID/build/$APP_DIR && $TF_PATH init',
+            'cd $TMPDIR/config0/$STATEFUL_ID/build/$APP_DIR && $TF_PATH refresh',
+            'cd $TMPDIR/config0/$STATEFUL_ID/build/$APP_DIR && $TF_PATH plan -detailed-exitcode'
         ]
 
         return cmds
