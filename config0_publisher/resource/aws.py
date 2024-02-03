f907397068144627007185f81866fadfe1882a70
gary <gary>
2024-02-03 17:25:30 +0800

    rendered and updated 02-03-2024-17-25

--- config0_publisher/resource/aws.py
+++ config0_publisher/resource/aws.py
@@ -34,10 +34,10 @@ class TFCmdOnAWS(object):
         if tf_bucket_path:
             cmds.extend([
                 f'mkdir -p $TMPDIR/downloads || echo "download directory exists"',
-                f'(cd $TMPDIR/downloads && ls terraform_{tf_version})' || \
-                f'(cd $TMPDIR/downloads && aws s3 cp {tf_bucket_path} terraform_{tf_version} --quiet) || \
-                   cd $TMPDIR/downloads && curl -L -s https://releases.hashicorp.com/terraform/{tf_version}/terraform_{tf_version}_linux_amd64.zip -o terraform_{tf_version} && \
-                   cd $TMPDIR/downloads && aws s3 cp terraform_{tf_version} {tf_bucket_path} --quiet'
+                f'(cd $TMPDIR/downloads && ls terraform_{tf_version}) || \
+                (cd $TMPDIR/downloads && aws s3 cp {tf_bucket_path} terraform_{tf_version} --quiet) || \
+                cd $TMPDIR/downloads && curl -L -s https://releases.hashicorp.com/terraform/{tf_version}/terraform_{tf_version}_linux_amd64.zip -o terraform_{tf_version} && \
+                cd $TMPDIR/downloads && aws s3 cp terraform_{tf_version} {tf_bucket_path} --quiet'
             ])
         else:
             cmds.extend([
