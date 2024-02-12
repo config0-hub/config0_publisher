35ea373777e78d392073dfa1e253c7bdd070cbd4
gary <gary>
2024-02-12 08:53:05 +0800

    rendered and updated 02-12-2024-08-53

--- config0_publisher/class_helper.py
+++ config0_publisher/class_helper.py
@@ -60,20 +60,25 @@ class SetClassVarsHelper:
         if not self.set_env_vars:
             return
 
-        for env_var,must_exists in self.set_env_vars.items():
+        env_vars_keys = self.set_env_vars.keys()
 
-            if env_var in self.kwargs:
-                self._vars_set[env_var] = self.kwargs[env_var]
-                _expression = f'self.{env_var}="{self.kwargs[env_var]}"'
-            elif env_var.upper() in self.env_vars:
-                self._vars_set[env_var] = self.env_vars[env_var.upper()]
-                _expression = f'self.{env_var}="{self.env_vars[env_var.upper()]}"'
-            elif env_var.upper() in os.environ:
-                self._vars_set[env_var] = os.environ[env_var.upper()]
-                _expression = f'self.{env_var}="{os.environ[env_var.upper()]}"'
-            elif env_var.lower() in self.default_values:
-                self._vars_set[env_var.lower()] = self.default_values[env_var.lower()]
-                _expression = f'self.{env_var.lower()}="{self.default_values[env_var.lower()]}"'
+        if self.default_values:
+            env_vars_keys.extend(self.default_values.keys())
+
+        for env_var_key in env_vars_keys:
+
+            if env_var_key in self.kwargs:
+                self._vars_set[env_var_key] = self.kwargs[env_var_key]
+                _expression = f'self.{env_var_key}="{self.kwargs[env_var_key]}"'
+            elif env_var_key.upper() in self.env_vars:
+                self._vars_set[env_var_key] = self.env_vars[env_var_key.upper()]
+                _expression = f'self.{env_var_key}="{self.env_vars[env_var_key.upper()]}"'
+            elif env_var_key.upper() in os.environ:
+                self._vars_set[env_var_key] = os.environ[env_var_key.upper()]
+                _expression = f'self.{env_var_key}="{os.environ[env_var_key.upper()]}"'
+            elif env_var_key.lower() in self.default_values:
+                self._vars_set[env_var_key.lower()] = self.default_values[env_var_key.lower()]
+                _expression = f'self.{env_var_key.lower()}="{self.default_values[env_var_key.lower()]}"'
             else:
                 _expression = None
 
@@ -88,17 +93,17 @@ class SetClassVarsHelper:
                 continue
 
             print("1"*32)
-            print(f"env_var {env_var}")
+            print(f"env_var {env_var_key}")
             print("2"*32)
             print(f"_expression {_expression}")
             print("3"*32)
 
-            if must_exists:
-                raise Exception("variable {} needs to be set".format(env_var))
+            if self.set_env_vars.get(env_var_key):  # must_exists
+                raise Exception("variable {} needs to be set".format(env_var_key))
 
             if self.set_default_null:
-                self._vars_set[env_var] = None
-                print("set None for variable {}".format(env_var))
-                exec('self.{}=None'.format(env_var))
+                self._vars_set[env_var_key] = None
+                print("set None for variable {}".format(env_var_key))
+                exec('self.{}=None'.format(env_var_key))
 
-        self._set_class_default_vars()
+        #self._set_class_default_vars()
