f803461ff5b07fc9c5d5fcadf9b9a1c9daaac6ec
gary <gary>
2024-03-02 19:00:26 +0800

    rendered and updated 03-02-2024-19-00

--- config0_publisher/resource/aws.py
+++ config0_publisher/resource/aws.py
@@ -14,8 +14,9 @@ class TFCmdOnAWS(object):
     def reset_dirs(self):
 
         #     'rm -rf $TMPDIR/config0 > /dev/null 2>&1 || echo "$TMPDIR/config0 does not exists"',
+        #    '[ ! -e $TMPDIR/config0 ] || rm -rf $TMPDIR/config0',
         cmds = [
-            '[ ! -e $TMPDIR/config0 ] || rm -rf $TMPDIR/config0',
+            '(ls $TMPDIR/config0 > /dev/null 2>&1 && rm -rf $TMPDIR/config0) || echo "directory $TMPDIR/config0 does not exists',
             'mkdir -p $TMPDIR/config0/$STATEFUL_ID/build'
         ]
 
