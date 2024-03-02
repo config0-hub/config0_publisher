ec22925e006c50b45c91a3b93f840e60273be129
gary <gary>
2024-03-02 13:05:53 +0800

    rendered and updated 03-02-2024-13-05

--- config0_publisher/resource/aws.py
+++ config0_publisher/resource/aws.py
@@ -28,7 +28,7 @@ class TFCmdOnAWS(object):
         dl_dir = '$TMPDIR/downloads'
 
         cmds = [ 
-            f'[[ ! -e {dl_dir} ]] || (for file in {dl_dir}/{tf_name}_*; do [[ $file != "{dl_dir}/{tf_name}_{tf_version}" ]] && echo "Deleting file: $file" && (rm "$file" || echo "could not remove $file") ; done)',
+            f'[[ ! -e {dl_dir} ]] || (for file in {dl_dir}/{tf_name}_*; do [[ $file != "{dl_dir}/{tf_name}_{tf_version}" ]] && echo "Deleting file: $file" && rm "$file" || echo "could not remove $file"; done)',
             f'mkdir -p "{dl_dir}" > /dev/null 2>&1 || echo "{dl_dir} already exists"',
             f'[[ ! -e "{dl_dir}/{tf_name}_{tf_version}" && ! -e "$TF_PATH" ]] && touch "{f_dne}"'
             ]
