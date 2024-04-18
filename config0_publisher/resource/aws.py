be20749463e14c9165cbc089ca5cfccb73f957cb
gary <gary>
2024-04-18 16:13:54 +0800

    rendered and updated 04-18-2024-16-13

--- config0_publisher/resource/aws.py
+++ config0_publisher/resource/aws.py
@@ -88,11 +88,10 @@ class TFCmdOnAWS(object):
             cmds = [
                 f'rm -rf $TMPDIR/config0/$STATEFUL_ID/{envfile_env} > /dev/null 2>&1 || echo "env file already removed"',
                 f'/tmp/decrypt -s $STATEFUL_ID -d $TMPDIR/config0/$STATEFUL_ID/{self.envfile} -e $TMPDIR/config0/$STATEFUL_ID/build/{envfile_env}.enc',
-                f'echo $SSM_NAME',
-                f'echo $SSM_NAME',
-                f'echo $SSM_NAME',
-                f'echo $SSM_NAME',
-                f'echo $SSM_NAME',
+                'if [ -n "$SSM_NAME" ]; then echo $SSM_NAME; fi',
+                'if [ -n "$SSM_NAME" ]; then echo $SSM_NAME; fi',
+                'if [ -n "$SSM_NAME" ]; then echo $SSM_NAME; fi',
+                'if [ -n "$SSM_NAME" ]; then echo $SSM_NAME; fi',
                 f'ssm_get -name $SSM_NAME -file $TMPDIR/config0/$STATEFUL_ID/{self.envfile} || echo "WARNING: SSM_NAME: $SSM_NAME not set or problems fetching it"'
             ]
 
