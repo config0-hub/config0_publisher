147004f8e8f888bf658e0f11037b6a0f5eab7dfa
gary <gary>
2024-09-03 22:56:14 +0800

    rendered and updated 09-03-2024-22-56

--- config0_publisher/resource/tfinstaller.py
+++ config0_publisher/resource/tfinstaller.py
@@ -12,7 +12,7 @@ def get_tf_install(**kwargs):
     dl_subdir = kwargs["dl_subdir"]
     bucket_path = kwargs["tf_bucket_path"]
     arch = kwargs["arch"]
-    bin_dir = kwargs["bin_dir"]
+    path_dir = kwargs["tf_path_dir"]
 
     if runtime_env == "codebuild":
         cmds = [
@@ -22,20 +22,20 @@ def get_tf_install(**kwargs):
     else:
         cmds = [f'echo "downloading {binary}_{version}"']
 
-    bucket_install = f'([ ! -f "$TMPDIR/{dl_subdir}/{binary}_{version}" ] && aws s3 cp {bucket_path} $TMPDIR/{dl_subdir}/{binary}_{version} --quiet )'
-    terraform_direct = f'(cd $TMPDIR/{dl_subdir} && curl -L -s https://releases.hashicorp.com/terraform/{version}/{binary}_{version}_{arch}.zip -o {binary}_{version} && aws s3 cp {binary}_{version} {bucket_path} --quiet)'
-    tofu_direct = f'cd $TMPDIR/{dl_subdir} && curl -L -s https://github.com/opentofu/opentofu/releases/download/v{version}/{binary}_{version}_{arch}.zip -o {binary}_{version} && aws s3 cp {binary}_{version} {bucket_path} --quiet'
+    bucket_install = f'aws s3 cp {bucket_path} $TMPDIR/{dl_subdir}/{binary}_{version} --quiet && echo "############" && echo \n"### GOT "{binary} from s3/cache\n && echo "############"'
+    terraform_direct = f'echo "########## NEED FRM SOURCE" && cd $TMPDIR/{dl_subdir} && curl -L -s https://releases.hashicorp.com/terraform/{version}/{binary}_{version}_{arch}.zip -o {binary}_{version} && aws s3 cp {binary}_{version} {bucket_path} --quiet'
+    tofu_direct = f'echo "############ NEED FRM SOURCE" && cd $TMPDIR/{dl_subdir} && curl -L -s https://github.com/opentofu/opentofu/releases/download/v{version}/{binary}_{version}_{arch}.zip -o {binary}_{version} && aws s3 cp {binary}_{version} {bucket_path} --quiet'
 
     if binary == "terraform":
-        _install_cmd = f'{bucket_install} || (echo "terraform/tofu not found in local s3 bucket" && {terraform_direct})'
+        _install_cmd = f'({bucket_install} )|| (echo "terraform/tofu not found in local s3 bucket" && {terraform_direct})'
     else:  # opentofu
-        _install_cmd = f'{bucket_install} || (echo "terraform/tofu not found in local s3 bucket" && {tofu_direct})'
+        _install_cmd = f'({bucket_install}) || (echo "terraform/tofu not found in local s3 bucket" && {tofu_direct})'
 
     cmds.append(_install_cmd)
 
     cmds.extend([
-        f'mkdir -p {bin_dir} || echo "trouble making bin_dir {bin_dir}"',
-        f'(cd $TMPDIR/{dl_subdir} && unzip {binary}_{version} && mv {binary} {bin_dir}/{binary} > /dev/null) || exit 0',
-        f'chmod 777 {bin_dir}/{binary}'])
+        f'mkdir -p {path_dir} || echo "trouble making path_dir {path_dir}"',
+        f'(cd $TMPDIR/{dl_subdir} && unzip {binary}_{version} && mv {binary} {path_dir}/{binary} > /dev/null) || exit 0',
+        f'chmod 777 {path_dir}/{binary}'])
 
     return cmds
