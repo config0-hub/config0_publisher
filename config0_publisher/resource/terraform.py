7fe8928d7e06f59d95f6439ed951fa45b627694b
gary <gary>
2024-09-07 17:49:40 +0800

    rendered and updated 09-07-2024-17-49

--- config0_publisher/resource/terraform.py
+++ config0_publisher/resource/terraform.py
@@ -119,8 +119,8 @@ class TFCmdOnAWS(TFAppHelper):
             # testtest456
             #ssm_cmd = f'if [ -f /{self.ssm_tmp_dir}/.ssm_value ]; then cd /{self.ssm_tmp_dir}/; . ./.ssm_value; fi'
             #ssm_cmd = f'if [ -f /{self.ssm_tmp_dir}/.ssm_value ]; then cd /{self.ssm_tmp_dir}/; set -a; . ./.ssm_value; set +a; fi;env| grep INFRA'
-            ssm_cmd = f'if [ -f /{self.ssm_tmp_dir}/.ssm_value ]; then cd /{self.ssm_tmp_dir}/; set -a; . ./.ssm_value; set +a; fi'
-            ssm_cmd = f'if [ -f /{self.ssm_tmp_dir}/.ssm_value ]; then cd /{self.ssm_tmp_dir}/; . ./.ssm_value; fi; env |grep INFRA'
+            #ssm_cmd = f'if [ -f /{self.ssm_tmp_dir}/.ssm_value ]; then cd /{self.ssm_tmp_dir}/; set -a; . ./.ssm_value; set +a; fi'
+            ssm_cmd = f'if [ -f /{self.ssm_tmp_dir}/.ssm_value ]; then cd /{self.ssm_tmp_dir}/; . ./.ssm_value; fi; env'
             self.src_env_files_cmd = f'{base_cmd}; {ssm_cmd}'
 
         return self.src_env_files_cmd
