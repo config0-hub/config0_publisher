552c893805842aa8f7b141ce9b99386c68dbe88c
gary <gary>
2024-09-08 17:25:31 +0800

    rendered and updated 09-08-2024-17-25

--- config0_publisher/resource/common.py
+++ config0_publisher/resource/common.py
@@ -101,7 +101,7 @@ class TFAppHelper:
 
         #_bucket_install = f'aws s3 cp {bucket_path} {dl_file_path} --quiet && echo "############" && echo "### GOT {base_file_path} from s3 bucket/cache ###" && echo "############"'
         #_src_install = f'echo "############" && echo "### NEED to get {base_file_path} from source ###" && echo "############" && curl -L -s {src_remote_path} -o {dl_file_path} && aws s3 cp {dl_file_path} {bucket_path} --quiet'
-        _bucket_install = f'aws s3 cp {bucket_path} {dl_file_path} --quiet && echo "### GOT {base_file_path} from s3 bucket/cache"'
+        _bucket_install = f'aws s3 cp {bucket_path} {dl_file_path} --quiet && echo "##########" && echo "### GOT {base_file_path} from s3 bucket/cache" && echo "###########"'
         _src_install = f'echo "### NEED to get {base_file_path} from source" && curl -L -s {src_remote_path} -o {dl_file_path} && aws s3 cp {dl_file_path} {bucket_path} --quiet'
         install_cmd = f'({_bucket_install}) || ({_src_install})'
 
