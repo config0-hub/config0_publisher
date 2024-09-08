924eca28cc7d3186164d044ffcc7750535443ca3
gary <gary>
2024-09-08 19:49:41 +0800

    rendered and updated 09-08-2024-19-49

--- config0_publisher/resource/infracost.py
+++ config0_publisher/resource/infracost.py
@@ -46,9 +46,10 @@ class TFInfracostHelper(TFAppHelper):
 
         #f'echo "executing INFRACOST without src_env_file"',
         #f'({self.base_cmd} --no-color breakdown --path . --format json --out-file {self.base_output_file}.json) || (echo "WARNING: looks like INFRACOST failed")',
+        #f'({self.base_cmd} --no-color breakdown --path . --out-file {self.base_output_file}.out && cat {self.base_output_file}.out ) || (echo "WARNING: looks like INFRACOST failed")'
 
         return [
-            f'({self.base_cmd} --no-color breakdown --path . --out-file {self.base_output_file}.out && cat {self.base_output_file}.out ) || (echo "WARNING: looks like INFRACOST failed")'
+            f'echo "executing INFRACOST without src_env_file"'
         ]
 
     def get_all_cmds(self,src_env_file=None):
