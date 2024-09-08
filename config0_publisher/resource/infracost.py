712ff0ab8c9ce055860e50f9727c4c4e54ed3e81
gary <gary>
2024-09-08 19:54:55 +0800

    rendered and updated 09-08-2024-19-54

--- config0_publisher/resource/infracost.py
+++ config0_publisher/resource/infracost.py
@@ -35,7 +35,7 @@ class TFInfracostHelper(TFAppHelper):
 
         return cmds
 
-    def exec_cmds(self,src_env_file=None):
+    def exec_cmds(self):
 
         #if src_env_file:
         #    return [
@@ -44,12 +44,11 @@ class TFInfracostHelper(TFAppHelper):
         #        f'({src_env_file} && {self.base_cmd} --no-color breakdown --path . --out-file {self.base_output_file}.out && cat {self.base_output_file}.out ) || (echo "WARNING: looks like INFRACOST failed")'
         #    ]
 
-        #f'echo "executing INFRACOST without src_env_file"',
         #f'({self.base_cmd} --no-color breakdown --path . --format json --out-file {self.base_output_file}.json) || (echo "WARNING: looks like INFRACOST failed")',
         #f'({self.base_cmd} --no-color breakdown --path . --out-file {self.base_output_file}.out && cat {self.base_output_file}.out ) || (echo "WARNING: looks like INFRACOST failed")'
 
         return [
-            f'echo "executing INFRACOST without src_env_file"'
+            f'({self.base_cmd} --no-color breakdown --path . --out-file {self.base_output_file}.out && cat {self.base_output_file}.out ) || (echo "WARNING: looks like INFRACOST failed")'
         ]
 
     def get_all_cmds(self,src_env_file=None):
