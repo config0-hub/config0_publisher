b1697261ed8e87efb17b365b303d4e6fe22eca97
gary <gary>
2025-06-17 22:32:31 +0800

    rendered and updated 06-17-2025-22-32

--- config0_publisher/resource/manage.py
+++ config0_publisher/resource/manage.py
@@ -1571,7 +1571,7 @@ class ResourceCmdHelper(ResourcePhases):
 
         #if tf_results.get("phases") and results.get("in_progress") and tf_results.get("status") is True:
 
-        if tf_results.get("phases") and tf_results.get("status") is True:
+        if tf_results.get("phases") and (not tf_results.get("done") or not tf_results.get("tf_status")):
             self.logger.debug("e2" * 32)
             self.write_phases_to_json_file(tf_results)
             return tf_results
