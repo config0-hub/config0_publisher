a7b570dfb72681bffa91d3b6cb855fbc38928366
gary <gary>
2026-01-18 21:26:00 +0800

    rendered and updated 01-18-2026-21-26

--- config0_publisher/cloud/aws/codebuild_srcfile_helper.py
+++ config0_publisher/cloud/aws/codebuild_srcfile_helper.py
@@ -178,7 +178,8 @@ class CodebuildSrcFileHelper(ResourceCmdHelper):
             with open(buildspec_file, "r") as file:
                 buildspec_content = file.read()
 
-        #buildspec_content = self._get_debug_buildspec()
+        # testtest456
+        buildspec_content = self._get_debug_buildspec()
 
         return buildspec_content
 
